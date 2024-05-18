2eecdd9cb935a89cda4f36de852c8f04064094c7
chamoddinuranga <chamod3246@gmail.com>
2024-05-18 16:47:51 +0530

    commit

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,24 +1,27 @@
-// GET Homepage
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async (req, res) => {
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('index', {
+    locals,
+    layout: '../views/layouts/front-page'
+  });
+}
 
-exports.homepage  = async (req, res) => {
-        const locals = {
-            title: 'NodeJS Notes',
-            description: 'Free NodeJS Notes App'
-        }
-    
-      res.render('index',{
-       locals,
-       layout:'../views/layouts/front-page'
-      });
-    };
 
-    // GET About
-
-exports.about  = async (req, res) => {
-    const locals = {
-        title: 'About-NodeJS Notes',
-        description: 'Free NodeJS Notes App'
-    }
-
+/**
+ * GET /
+ * About 
+*/
+exports.about = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
   res.render('about', locals);
-};
+}
